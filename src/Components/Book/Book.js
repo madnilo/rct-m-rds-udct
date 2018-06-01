faba71f92d98426eb89339128f47466be56dd620
Danilo Moura <danilomourag@gmail.com>
2018-06-01 19:07:07 -0300

    Fix missing book cover problem

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './Book.css';
 
 const Book = (props) => {
@@ -7,7 +6,7 @@ const Book = (props) => {
 	return (
 		<div className="book">
 			<div className="book-top">
-				<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+				<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : ''}")` }}></div>
 				<div className="book-shelf-changer">
 					<select onChange={(e) => props.changeShelf(book, e)} value={book.shelf}>
 						<option value="0" disabled>Move to...</option>
